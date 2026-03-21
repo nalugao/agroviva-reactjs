b731fbad4b4df8f2f6d28f57ae12af1612797b28
Jeffergs <jeffersongs1995@gmail.com>
2026-03-21 19:49:41 -0300

    ajustes de estrutura

--- agroviva/src/components/Layout.jsx
+++ agroviva/src/components/Layout.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./Navbar";
-import Footer from ".././components/Footer/Footer";
+import Navbar from "./navbar";
+import Footer from "./footer/footer";
 
 import { Outlet } from "react-router-dom"
 
